d5af8ac78d84cdeb4bcb347733aca30638475126
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-04-09 15:07:04 +0100

    Homework start

---
+++ specs/park.test.js
@@ -0,0 +1,42 @@
+const Park = require('../park.js');
+const Dinosaur = require('../dinosaur.js');
+
+describe('Park', function() {
+
+  beforeEach( () => {
+    // write your code here
+  })
+
+  xtest('should have a name', () => {
+    // write your code here
+  });
+
+  xtest('should have a ticket price', () => {
+    // write your code here
+  });
+
+  xtest('should have a collection of dinosaurs', () => {
+    // write your code here
+  });
+
+  xtest('should be able to add a dinosaur to its collection', () => {
+    // write your code here
+  });
+
+  xtest('should be able to remove a dinosaur from its collection', () => {
+    // write your code here
+  });
+
+  xtest('should be able to find the dinosaur that attracts the most visitors', () => {
+    // write your code here
+  });
+
+  xtest('should be able to find all dinosaurs of a particular species', () => {
+    // write your code here
+  });
+
+  test('should be able to remove all dinosaurs of a particular species', () => {
+    // write your code here
+  });
+
+});
